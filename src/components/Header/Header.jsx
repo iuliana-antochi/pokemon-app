24e966b2b93750d6786ba81b6dd46161ef84b542
Catalina Hasnas <catalina.hasnas@opswat.com>
2024-06-05 17:26:08 +0300

    added feedback

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,6 +3,9 @@ import logo from "../../assets/pokemon-6.svg"
 
 function Header() {
   return (
+    // There is no need for empty tags <>;
+    // They exist because each React Component needs to have one parent;
+    // This one already has <header> tag that is a parent.
     <>
       <header>
         <img className="logo" src={logo}></img>
